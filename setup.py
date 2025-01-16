ef11f7549527d5729a036425e00567c374b04472
Drew DeHaas <drewdehaas@gmail.com>
2025-01-16 10:02:41 -0500

    Auto-install requirements via pip, add GitHub workflow
    * The dependences in requirements.txt are auto-installed (or checked)
      when `pip install` is used.
    * Add GitHub workflow for building packages in a variety of Python
      releases, run flake8 + formatting.

--- setup.py
+++ setup.py
@@ -1,7 +1,12 @@
 from setuptools import setup, find_packages
+import os
 
 PACKAGE_NAME = "grg_pheno_sim"
 VERSION = "1.0"
+
+THISDIR = os.path.dirname(os.path.realpath(__file__))
+with open(os.path.join(THISDIR, "requirements.txt")) as f:
+    requirements = list(map(str.strip, f))
 
 setup(
     name=PACKAGE_NAME,
@@ -13,5 +18,8 @@ setup(
     url="https://aprilweilab.github.io/",
     classifiers=[
         "Programming Language :: Python :: 3",
+    ],
+    install_requires=[
+        requirements,
     ],
 )
